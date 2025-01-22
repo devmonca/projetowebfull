01480ae88d38368a337099dc5e7b096ed6e233d4
gabriel <devmonca@gmail.com>
2025-01-22 08:04:32 -0300

    Cyprograph failed

--- back/src/main/java/com/example/criandoapi/projeto/service/UsuarioService.java
+++ back/src/main/java/com/example/criandoapi/projeto/service/UsuarioService.java
@@ -2,6 +2,8 @@ package com.example.criandoapi.projeto.service;
 
 import com.example.criandoapi.projeto.model.Usuario;
 import com.example.criandoapi.projeto.repository.IUsuario;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,9 +11,11 @@ import java.util.List;
 @Service
 public class UsuarioService {
     private IUsuario repository;
+    private PasswordEncoder passwordEncoder;
 
     public UsuarioService(IUsuario repository) {
         this.repository = repository;
+        this.passwordEncoder= new BCryptPasswordEncoder();
     }
 
     public List<Usuario> listarUsuario(){
@@ -20,11 +24,15 @@ public class UsuarioService {
     }
 
     public Usuario criarUsuario(Usuario usuario){
+        String encoder = this.passwordEncoder.encode(usuario.getEmail());
+        usuario.setEmail(encoder);
         Usuario usuarioNovo = repository.save(usuario);
         return usuarioNovo;
     }
 
     public Usuario editarUsuario(Usuario usuario){
+        String encoder = this.passwordEncoder.encode(usuario.getEmail());
+        usuario.setEmail(encoder);
         Usuario usuarioAtualizado = repository.save(usuario);
         return usuarioAtualizado;
     }
@@ -35,4 +43,10 @@ public class UsuarioService {
     }
 
 
+    public Boolean validarEmail(Usuario usuario) {
+        String email = repository.getById(usuario.getId()).getEmail();
+        Boolean valid = passwordEncoder.matches(usuario.getEmail(),email);
+        Boolean valid2 = usuario.getEmail()== email ? true : false;
+        return valid2;
+    }
 }
